54f0dff8dc8e2cb0a0ce87476f635e45c67b6558
Jose Baena <jobaco04@hotmail.com>
2018-12-19 19:51:37 +0100

    Added auxiliar functions and support for the DB

--- Prácticas/Tests/server.py
+++ Prácticas/Tests/server.py
@@ -25,9 +25,13 @@
 ################################################################################
 
 import socket
+import sqlite3
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 5000        # Port to listen on (non-privileged ports are > 1023)
+
+conn_db = sqlite3.connect('formulario.db')
+c = conn_db.cursor()
 
 class Usuario:
     nombre = None;
@@ -46,33 +50,67 @@ def user_exists(nombre):
             return True
     return False
 
-#Sin implementar
-def check_pass(user, pass):
-    return
+def check_pass(user, password):
+    c.execute("SELECT password FROM Usuarios WHERE user=\'%s\'" % user)
+    if c.fetchone()[0] == password:
+        return True
+    return False
 
-#Sin implementar
 def check_is_admin(user):
-    return
+    c.execute("SELECT password FROM Usuarios WHERE user=\'%s\'" % user)
+    if c.fetchone()[0] == '1':
+        return True
+    return False
 
-#Sin implementar
 def get_user(addr):
-    return
+    for x in usuarios:
+        if x.ip == addr:
+            return True
+    return False
 
-#Sin implementar
 def check_form_exists(nombre):
-    return
+    c.execute("SELECT * FROM Formularios WHERE nombre=\'%s\'" % nombre)
+    if c.fetchone() is None:
+        return True
+    return False
 
-#Sin implementar
 def crear_formulario(nombre):
-    return
+    if not check_form_exists(nombre):
+        c.execute('INSERT into Formularios values(?,null,null,null,null,null)', (nombre))
+        return True
+    return False
 
-#Sin implementar
-def add_question(nombre):
-    return
+def add_question(nombre, pregunta):
+    c.execute("SELECT * FROM Formularios WHERE nombre=\'%s\'" % nombre)
+    indice = 1
+    if c.fetchone()[2] is "null":
+        break
+    if c.fetchone()[3] is "null":
+        indice = 2
+        break
+    if c.fetchone()[4] is "null":
+        indice = 3
+        break
+    if c.fetchone()[5] is "null":
+        indice = 4
+        break
+    indice = 'pregunta' + str(indice)
+    if not check_form_exists(nombre):
+        c.execute('UPDATE into Formularios SET %s=%s' % indice, pregunta)
+        return True
+    return False
 
-#Sin implementar
 def check_question_exists(nombre, pregunta):
-    return
+    c.execute("SELECT * FROM Formularios WHERE nombre=\'%s\'" % nombre)
+    if c.fetchone()[2] == pregunta:
+        return True
+    if c.fetchone()[3] == pregunta:
+        return True
+    if c.fetchone()[4] == pregunta:
+        return True
+    if c.fetchone()[5] == pregunta:
+        return True
+    return False
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
